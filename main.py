ca0c671617d1b4c1ed8585b0e016b64bd0009030
pushkarjain1009 <pushkarjain1009@gmail.com>
2021-04-09 00:40:02 +0530

    Added Visualisation

--- main.py
+++ main.py
@@ -7,41 +7,51 @@ class Classifier(Forward, Backward):
         super(Classifier, self).__init__()
 
         self.layer_dims = layers
-        self.losses = []
-        self.accuracies = []
+        self.train_losses = []
+        self.train_accuracy = []
+        self.val_losses = []
+        self.val_accuracy = []
         self.parameters = Forward().initialize_parameters(layer_dims=self.layer_dims)
-        
-    def train(self, X, y, epoches, learning_rate):
+
+    def train(self, X_train, y_train, X_val, y_val, epoches, learning_rate):
 
         for i in range(0, epoches+1):
 
-            out, caches = Forward().model_forward(X, self.parameters)
-            loss = Forward().compute_loss(out, y)
-            _, acc = Forward().compute_accuracy(out, y)
-            grads =  Backward().model_backward(out, y, caches)            
+            # Training
+            out, caches = Forward().model_forward(X_train, self.parameters)
+            train_loss = Forward().compute_loss(out, y_train)
+            train_acc = Forward().compute_accuracy(out, y_train)
+            grads =  Backward().model_backward(out, y_train, caches)            
             self.parameters = Backward().update_parameters(self.parameters, grads, learning_rate)
+
+            # Validation
+            out, caches = Forward().model_forward(X_val, self.parameters)
+            val_loss = Forward().compute_loss(out, y_val)
+            val_acc = Forward().compute_accuracy(out, y_train)
 
             if i% 50 == 0:
 
-                print("Epoch {} =>  loss : {loss:.2f};   Accuracy : {acc:.2f}%;   ".format(i, loss=loss, acc=acc, ))
+                print("Epoch {} =>  Training Loss : {t_loss:.2f};   Training Acc : {t_acc:.2f}%   Validation Loss : {v_loss:.2f};   Validation Acc : {v_acc:.2f}%;   ".format(i, t_loss=train_loss, t_acc=train_acc, v_loss=val_loss, v_acc=val_acc,))
 
-                self.losses.append(loss)
-                self.accuracies.append(acc)
-        
-        return self.losses, self.accuracies
-                
+                self.train_losses.append(train_loss)
+                self.train_accuracy.append(train_acc)
+                self.val_losses.append(val_loss)
+                self.val_accuracy.append(val_acc)
+
+        return self.train_losses, self.train_accuracy, self.val_losses, self.val_accuracy
+
     def predict(self, X, y):
-        
+
         m = X.shape[1]
         n = len(self.parameters) // 2 # number of layers in the neural network
         p = np.zeros((1,m))
-        
+
         # Forward() propagation
         prediction_array, caches = Forward().model_forward(X, self.parameters)
 
-        p, acc = Forward().compute_accuracy(prediction_array, y)
-            
-        return prediction_array, p, acc
+        acc = Forward().compute_accuracy(prediction_array, y)
+
+        return prediction_array, acc
 
 
 def main():
@@ -49,35 +59,35 @@ def main():
     X_train = np.load("dataset/X_train.npy")
     y_train = np.load("dataset/y_train.npy")
 
-    classes = ['Cat', 'Non Cat']
+    X_test = np.load("dataset/X_test.npy")
+    y_test = np.load("dataset/y_test.npy")
+
+    classes = ['Non-Cat', 'Cat']
     layer_dims = [12288, 20, 7, 5, 1]
-    epoches= 1000
-    learning_rate = 0.0075
+    epoches = 1000
+    learning_rate = 0.01
 
     clf = Classifier(layer_dims)
-    """vis = Visualize()
+    vis = Visualize()
 
-    vis.data_visualize(X_train, y_train)"""
+    vis.data_visualize(X_train, y_train)
 
     X_train = X_train.reshape(X_train.shape[0], -1).T
 
     X_train = X_train/255.
 
-    loss, acc = clf.train(X_train, y_train, epoches, learning_rate)
-
-    """vis.plot(loss, 'Loss', 'Loss vs Epoches')
-    vis.plot(acc, 'Accuracy', 'Accuracy vs Epoches')"""
-
-    X_test = np.load("dataset/X_test.npy")
-    y_test = np.load("dataset/y_test.npy")
-
     X_test_ = X_test.reshape(X_test.shape[0], -1).T
 
     X_test_ = X_test_/255.
 
-    prediction_array, y_pred, acc = clf.predict(X_test_, y_test)
+    train_loss, train_acc, val_loss, val_acc = clf.train(X_train, y_train, X_test_, y_test, epoches, learning_rate)
 
-    #vis.test_plot(X_test, y_test, y_pred, prediction_array, classes)
+    vis.plot(train_loss, val_loss, 'Loss', ["Training Loss", "Validation Loss"], 'Loss vs Epoches')
+    vis.plot(train_acc, val_acc, 'Accuracy', ["Training Accuracy", "Validation Accuracy"], 'Accuracy vs Epoches')
+
+    predictions_array, acc = clf.predict(X_test_, y_test)
+
+    vis.test_plot(X_test, y_test, predictions_array, classes)
 
     print("\n\nAccuracy of Classifier on Test Dataset: {acc: .2f}%".format(acc = acc*100))
 
--- utils.py
+++ utils.py
@@ -3,42 +3,42 @@ import matplotlib.pyplot as plt
 
 class Activation_Function:
     def sigmoid(self, Z):
-        
-        A = 1/(1.1+np.exp(-Z))
+
+        A = 1/(1+np.exp(-Z))
         cache = Z
-        
+
         return A, cache
 
     def relu(self, Z):
-        
+
         A = np.maximum(0,Z)
-        
+
         assert(A.shape == Z.shape)
-        
+
         cache = Z 
         return A, cache
 
 
     def relu_backward(self, dA, cache):
-        
+
         Z = cache
         dZ = np.array(dA, copy=True) # just converting dz to a correct object.
 
         dZ[Z <= 0] = 0
-        
+
         assert (dZ.shape == Z.shape)
-        
+
         return dZ
 
     def sigmoid_backward(self, dA, cache):
-        
+
         Z = cache
 
-        s = 1/(1.1+np.exp(-Z))
+        s = 1/(1+np.exp(-Z))
         dZ = dA * s * (1-s)
-        
+
         assert (dZ.shape == Z.shape)
-        
+
         return dZ
 
 
@@ -47,7 +47,7 @@ class Activation_Function:
 class Forward(Activation_Function):
 
     def initialize_parameters(self, layer_dims):
-        
+
         np.random.seed(1)
 
         parameters = {}
@@ -57,20 +57,20 @@ class Forward(Activation_Function):
         for l in range(1, L):
             parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1]) 
             parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
-            
+
             assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
             assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
 
-            
+
         return parameters
 
     def linear_forward(self, A, W, b):
-        
+
         Z = W.dot(A) + b
-        
+
         assert(Z.shape == (W.shape[0], A.shape[1]))
         cache = (A, W, b)
-        
+
         return Z, cache
 
     def linear_activation_forward(self, A_prev, W, b, activation):
@@ -79,7 +79,7 @@ class Forward(Activation_Function):
 
             Z, linear_cache = self.linear_forward(A_prev, W, b)
             A, activation_cache = Activation_Function().sigmoid(Z)
-        
+
         elif activation == "relu":
 
             Z, linear_cache = self.linear_forward(A_prev, W, b)
@@ -91,30 +91,30 @@ class Forward(Activation_Function):
         return A, cache
 
     def model_forward(self, X, parameters):
-        
+
         caches = []
         A = X
         L = len(parameters) // 2                  # number of layers in the neural network
-        
+
         for l in range(1, L):
             A_prev = A 
             A, cache = self.linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], activation = "relu")
             caches.append(cache)
-        
+
         AL, cache = self.linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation = "sigmoid")
         caches.append(cache)
-        
+
         assert(AL.shape == (1,X.shape[1]))
 
         return AL, caches
 
     def compute_loss(self, AL, Y):
-        
+
         m = Y.shape[0]
         loss = (1./m) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
         loss = np.squeeze(loss)      
         assert(loss.shape == ())
-        
+
         return loss
 
     def compute_accuracy(self, probas, y):
@@ -127,9 +127,9 @@ class Forward(Activation_Function):
                 p[0,i] = 1
             else:
                 p[0,i] = 0
-        
 
-        return p, np.sum((p == y)/m)
+
+        return np.sum((p == y)/m)
 
         
 ################################################################################################################
@@ -144,40 +144,40 @@ class Backward(Activation_Function):
         dW = 1./m * np.dot(dZ,A_prev.T)
         db = 1./m * np.sum(dZ, axis = 1, keepdims = True)
         dA_prev = np.dot(W.T,dZ)
-        
+
         assert (dA_prev.shape == A_prev.shape)
         assert (dW.shape == W.shape)
         assert (db.shape == b.shape)
-        
+
         return dA_prev, dW, db
 
     def linear_activation_backward(self, dA, cache, activation):
         linear_cache, activation_cache = cache
-        
+
         if activation == "relu":
             dZ = super().relu_backward(dA, activation_cache)
             dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
-            
+
         elif activation == "sigmoid":
             dZ = super().sigmoid_backward(dA, activation_cache)
             dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
-        
+
         return dA_prev, dW, db
 
     def model_backward(self, AL, Y, caches):
-        
+
         grads = {}
         L = len(caches) 
         m = AL.shape[1]
         Y = Y.reshape(AL.shape) 
-        
+
         dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-        
+
         current_cache = caches[L-1]
         grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = self.linear_activation_backward(dAL, current_cache, activation = "sigmoid")
-        
+
         for l in reversed(range(L-1)):
-            
+
             current_cache = caches[l]
             dA_prev_temp, dW_temp, db_temp = self.linear_activation_backward(grads["dA" + str(l + 1)], current_cache, activation = "relu")
             grads["dA" + str(l)] = dA_prev_temp
@@ -211,53 +211,66 @@ class Visualize:
             plt.title("Ground Truth: {}".format(y[i]))
         plt.show()
 
-    def plot(self, arr, y_label, title):
+    def plot(self, arr1, arr2, y_label, legend, title):
 
-        x = len(arr)
-        plt.plot(range(x),arr)
+        x = len(arr1)
+
+        plt.plot([i*50 for i in range(x)], arr1)
+        plt.plot([i*50 for i in range(x)], arr2)
+
         plt.xlabel('Epoch')
         plt.ylabel(y_label)
+        plt.legend(legend)
         plt.title(title)
         plt.show()
 
     def plot_image(self, i, predictions_array, true_label, img, classes):
 
-        predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+        prediction_array, true_label, img = predictions_array[0][i], true_label[i], img[i]
         plt.grid(False)
         plt.xticks([])
         plt.yticks([])
 
         plt.imshow(img, cmap = plt.cm.binary)
 
-        predicted_label = np.argmax(predictions_array)
+        if prediction_array >= 0.5:
+            predicted_label = 1
+        else:
+            predicted_label = 0
+
         if predicted_label == true_label:
-            color = 'white'
+            color = 'green'
         else:
             color = 'red'
 
         plt.xlabel("{} {:2.0f}% ({})".format(classes[predicted_label],
-                                            100*np.max(predictions_array),
+                                            100*np.max([prediction_array, 1-prediction_array]),
                                             classes[true_label]),
                                             color=color)
         return
 
     def plot_value_array(self, i, predictions_array, true_label):
-        prediction_array, true_label = predictions_array[i], true_label[i]
+
+        prediction_array, true_label = predictions_array[0][i], true_label[i]
 
         plt.grid(False)
         plt.xticks([])
         plt.yticks([])
-        thisplot = plt.bar(range(2), prediction_array, color="#777777")
+        thisplot = plt.bar(range(2), [1-prediction_array, prediction_array], color="#777777")
         plt.ylim([0,1])
-        predicted_label = np.argmax(prediction_array)
+
+        if prediction_array >= 0.5:
+            predicted_label = 1
+        else:
+            predicted_label = 0
 
         thisplot[predicted_label].set_color('red')
         thisplot[true_label].set_color('green')
 
         return
 
-    def test_plot(self, X_test, y_test, y_pred, predictions_array, classes):
-        
+    def test_plot(self, X_test, y_test, predictions_array, classes):
+
         num_rows = 3
         num_cols = 3
         num_images = num_rows*num_cols
@@ -267,9 +280,9 @@ class Visualize:
         for i in range(num_images):
 
             plt.subplot(num_rows, 2*num_cols, 2*i+1)
-            self.plot_image(i, y_pred, y_test, X_test, classes)
+            self.plot_image(i, predictions_array, y_test, X_test, classes)
             plt.subplot(num_rows, 2*num_cols, 2*i+2)
-            self.plot_value_array(i,y_pred, y_test)
+            self.plot_value_array(i, predictions_array, y_test)
             plt.xticks(range(2), classes)
 
         plt.show()
